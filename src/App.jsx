e78ede53d3343d9d0016d1ca6185deb7d694cb36
AbuShahma022 <abushahma1014@gmail.com>
2025-11-02 11:34:12 +0600

    toast fix

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <BrowserRouter>
-    <Toaster position="top-right" />
+    <Toaster position="bottom-center" />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
